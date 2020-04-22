ab316d0535daa80ebc3d8253a1861a781f7faec7
Joaquin Diaz <joacod@gmail.com>
2020-04-22 12:33:13 +0200

    first commit

---
+++ NetCore.Playground.Api/Controllers/LyricsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Playground.Api.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NetCore.Playground.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    public class LyricsController : ControllerBase
+    {
+        public LyricsController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<SongLyrics> Get([Required]string artist, [Required]string song)
+        {
+            var apiURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
+            var textInfo = CultureInfo.CurrentCulture.TextInfo;
+            var lyrics = new SongLyrics
+            {
+                Artist = textInfo.ToTitleCase(artist),
+                SongTitle = textInfo.ToTitleCase(song),
+                Lyrics = ""
+            };
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(apiURL);
+            if (response.IsSuccessStatusCode)
+            {
+                lyrics.Lyrics = await response.Content.ReadAsStringAsync();
+            }
+
+            return lyrics;
+        }
+
+    }
+}
---
+++ NetCore.Playground.Api/Models/SongLyrics.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore.Playground.Api.Models
+{
+    public class SongLyrics
+    {
+        public string Artist { get; set; }
+        public string SongTitle { get; set; }
+        public string Lyrics { get; set; }
+
+    }
+}
